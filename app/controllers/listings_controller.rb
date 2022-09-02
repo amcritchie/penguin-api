cbeddbd82bc142640684c51c160c8eee790a08cd
Alex <alex@planomatic.com>
2022-09-02 12:44:07 -0600

    Adding development flag in html title to distinguish production easily

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,16 +3,9 @@ class ListingsController < ApplicationController
   def new
     # Format API payload
     payload = params['payload']
-    logger.info "="*40
-    logger.info "Confirming logger.info works"*40
-    # logger.ap "Confirming logger.ap works"*40
-    puts "Confirming puts works"*40
-    logger.info "="*40
-    # Puts payload information
-    logger.info "="*40
-    logger.info payload
-    logger.info "-"*40
-
+    puts "="*40
+    puts payload
+    puts "-"*40
 
     # Process payload into listing
     listing = Listing.process_payload(payload)
@@ -20,11 +13,11 @@ class ListingsController < ApplicationController
     return not_nfl_all_day_transaction(listing) unless listing.contract_slug == "nfl_all_day"
 
     # Puts data about listing transaction
-    logger.info "Transaction: #{listing.transaction_id}"
-    logger.info "Events: #{listing.events_count}"
-    logger.info "Event [0] NFT Serial: #{listing.nft_serial}"
-    logger.info "Event [0] NFT Price: #{listing.price}"
-    logger.info "-"*40
+    puts "Transaction: #{listing.transaction_id}"
+    puts "Events: #{listing.events_count}"
+    puts "Event [0] NFT Serial: #{listing.nft_serial}"
+    puts "Event [0] NFT Price: #{listing.price}"
+    puts "-"*40
 
     # Do something if moment is interesting...
 
